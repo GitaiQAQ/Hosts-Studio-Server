e614c7753a6b33000c0dc27e6d6cc1ff02aa14ee
gitai <gitai@qq.com>
2016-04-16 09:07:05 +0800

    first commit

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ var config = {
 	host:"http://hostseditor.avosapps.com",
 	port:8080,
 
-	av_app_id:"",
-	av_app_key:"",
-	av_master_key:"",
+	av_app_id:"sgyqr6wbvjgvlwwsntucqv7ip3q6gxfi30bxdgb9ui8ttxgs",
+	av_app_key:"g1ivn87jykoqk5o05bi6edw8uyqveix1af66n0dt68xkk70q",
+	av_master_key:"27amqhc8h3vttlq3s36v623m0gwgmhdg6qx6g6xs3hw8jpdb",
 
-	qiniu_ak:'',
-	qiniu_sk:'',
+	qiniu_ak:'YeL_Hx8uhZGDGqn83wo3yiP0ZuF0my5j-AcH5fPd',
+	qiniu_sk:'68WYcW2q97RCV7rz8XWaHvyCLkITd_NaIbxq7TbF',
 	qiniu_returnUrl:null,
 
-	secret:""
+	secret:"1f&v(a*s8fiu7&^h$af6$kf4f7q"
 }
 
 module.exports = config;
--- tingyun.js
+++ tingyun.js
@@ -1,5 +1,5 @@
 exports.config = {
-    app_name        : [""],
-    licenseKey      : "",
+    app_name        : ["Hosts Editor"],
+    licenseKey      : "dc7a9d3e5a7b3e2001ef384184aba1c0",
     agent_log_level : "info"
 };
